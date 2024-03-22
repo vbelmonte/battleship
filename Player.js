63760ab571d05feb441fe67cf063120da91b1189
Villy Belmonte <vjbbelmont@gmail.com>
2024-03-22 13:49:29 -0700

    Implement Player.js

--- Player.js
+++ Player.js
@@ -1,3 +1,20 @@
-export default function player() {
+import Gameboard from "./Gameboard"
 
+export default function player() {
+  let gameBoard = Gameboard();
+  let turn = false;
+
+  const getGameBoard = () => {
+    return gameBoard;
+  }
+
+  const setTurn = (state) => {
+    turn = state;
+  }
+
+  const getTurn = () => {
+    return turn;
+  }
+
+  return { getGameBoard, setTurn, getTurn }
 }
