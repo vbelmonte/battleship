546570123a87b0221750041fdefa1eab52c2c817
Villy Belmonte <vjbbelmont@gmail.com>
2024-03-26 17:52:25 -0700

    Revise tests for Player

--- Player.test.js
+++ Player.test.js
@@ -31,10 +31,6 @@ test("It is Player 1's turn, which should return true", () => {
 test('Player 1 sends an attack to computer, which is counted as a "miss"/false', () => {
   const player1 = Object.create(player());
   const computer = Object.create(player());
-  const compGameBoard = computer.getGameBoard();
-
-  const mockReceiveAttack = jest.fn((x, y) => compGameBoard.receiveAttack(x, y));
-  computer.receiveAttack = mockReceiveAttack;
 
   const mockSendAttack = jest.fn(opponent => opponent.receiveAttack(1, 1));
   player1.sendAttack = mockSendAttack;
@@ -46,10 +42,6 @@ test('Player 1 sends an attack to computer, which is counted as a "miss"/false',
 test('Player 1 sends an attack to computer, which is counted as a "miss"/false, and results in a turn for computer', () => {
   const player1 = Object.create(player());
   const computer = Object.create(player());
-  const compGameBoard = computer.getGameBoard();
-
-  const mockReceiveAttack = jest.fn((x, y) => compGameBoard.receiveAttack(x, y));
-  computer.receiveAttack = mockReceiveAttack;
 
   const mockSendAttack = jest.fn(opponent => opponent.receiveAttack(1, 1));
   player1.sendAttack = mockSendAttack;
@@ -76,12 +68,8 @@ test('Player 1 sends an attack to computer, which is counted as a "miss"/false, 
 test('Computer receives an attack, which is counted as a "hit"/true, and results in another turn for Player 1', () => {
   const player1 = Object.create(player());
   const computer = Object.create(player());
-  const compGameBoard = computer.getGameBoard();
-  
-  compGameBoard.placeShip(1, 1, 1, "horizontal");
 
-  const mockReceiveAttack = jest.fn((x, y) => compGameBoard.receiveAttack(x, y));
-  computer.receiveAttack = mockReceiveAttack;
+  computer.getGameBoard().placeShip(1, 1, 1, "horizontal");
 
   const mockSendAttack = jest.fn(opponent => opponent.receiveAttack(1, 1));
   player1.sendAttack = mockSendAttack;
@@ -107,15 +95,13 @@ test('Computer receives an attack, which is counted as a "hit"/true, and results
   expect(computer.getTurn()).not.toBeTruthy();
 });
 
+
 test('(1,1) on grid is already hit. Computer knows to send an attack to an available square (2,2) by using a list of available squares', () => {
   const player1 = Object.create(player());
   const computer = Object.create(player());
   const p1GameBoard = player1.getGameBoard();
 
   p1GameBoard.placeShip(1, 1, 1, "horizontal");
-
-  const mockReceiveAttack = jest.fn((x, y) => p1GameBoard.receiveAttack(x, y));
-  player1.receiveAttack = mockReceiveAttack;
 
   const mockSendAttack = jest.fn((opponent, x, y) => opponent.receiveAttack(1, 1));
   computer.sendAttack = mockSendAttack;
